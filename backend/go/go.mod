1963c64ed53854d37d6ef1a1dce6bba3cf7e9d9f
Bartosz Piechnik <bpiechnikgh@gmail.com>
2025-05-25 16:41:59 +0200

    Add CORS

--- backend/go/go.mod
+++ backend/go/go.mod
@@ -5,6 +5,7 @@ go 1.24.3
 require (
 	github.com/caarlos0/env/v11 v11.3.1
 	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-chi/cors v1.2.1
 	github.com/go-chi/render v1.0.3
 	github.com/go-playground/validator/v10 v10.26.0
 	github.com/golang-migrate/migrate/v4 v4.17.0
@@ -16,6 +17,7 @@ require (
 	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.33.0
+	google.golang.org/grpc v1.72.1
 	google.golang.org/protobuf v1.36.6
 )
 
@@ -41,6 +43,5 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
-	google.golang.org/grpc v1.72.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/go/internal/server/server.go
+++ backend/go/internal/server/server.go
@@ -1,13 +1,13 @@
 package server
 
 import (
-	"time"
 	"ztp/internal/config"
 	documentintelligence "ztp/internal/handlers/document_intelligence"
 	"ztp/internal/handlers/user"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
 	"github.com/jackc/pgx/v5/pgxpool"
 
@@ -27,11 +27,8 @@ func New(config *config.Config, pool *pgxpool.Pool) *Server {
 	handlers := user.NewUserService(repo)
 	ocr := documentintelligence.NewOcrService(repo)
 
-	router.Use(middleware.Timeout(time.Duration(config.ServerConfig.Timeout) * time.Second))
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
-	router.Use(middleware.RequestID)
-	router.Use(render.SetContentType(render.ContentTypeJSON))
+	configureRouter(router)
+
 	return &Server{
 		Router:     router,
 		repository: repo,
@@ -45,5 +42,20 @@ func (s *Server) ConfigureHandlers() {
 	s.Router.Get("/user/{username}", s.users.HandleGetByUsername)
 	s.Router.Get("/user", s.users.HandleGetByEmail)
 	s.Router.Post("/login", s.users.HandleLogin)
-	s.Router.Post("/document/{id}", s.ocrService.HandleDetectDocumentText)
+	s.Router.Post("/document/ocr/{id}", s.ocrService.HandleDetectDocumentText)
+}
+
+func configureRouter(router *chi.Mux) {
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.RequestID)
+	router.Use(render.SetContentType(render.ContentTypeJSON))
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 }
